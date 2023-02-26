a683fb925d1b670d59f8517159f989ee119d137d
JOSE CARLOS GUERRA MARTINEZ <jose.guerra.m@ibm.com>
2023-02-26 16:43:16 -0500

    terraform code modified

--- config/lamp.sh
+++ config/lamp.sh
@@ -2,7 +2,7 @@ export TF_VAR_ibmcloud_api_key="TZkW0e3U9BKJ3nEkp90wZMbRPmfYK07JO7NytkRrtxdO"
 
 export TF_VAR_vpc_ssh_key="~/.ssh/id_rsa"
 
-export TF_VAR_resource_group="ConsumptionSSA-Demos"
+export TF_VAR_resource_group="default"
 
 export TF_VAR_vpc_region="us-south"
 
