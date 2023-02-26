13431486c22935b006c2a600a74e6d16116369c1
José Guerra <72484685+JoCGM09@users.noreply.github.com>
2023-02-26 16:23:04 -0500

    Add files via upload

---
+++ config/lamp.sh
@@ -0,0 +1,11 @@
+export TF_VAR_ibmcloud_api_key="TZkW0e3U9BKJ3nEkp90wZMbRPmfYK07JO7NytkRrtxdO"
+
+export TF_VAR_vpc_ssh_key="~/.ssh/id_rsa"
+
+export TF_VAR_resource_group="ConsumptionSSA-Demos"
+
+export TF_VAR_vpc_region="us-south"
+
+export TF_VAR_resources_prefix="lamp"
+
+export TF_VAR_byok_data_volume=false
